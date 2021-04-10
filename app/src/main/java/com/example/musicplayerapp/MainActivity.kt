99104afc2cb8a821323848369717f50f3b23e08a
Trần Thị Hoa Hiên <56663780+bingo213@users.noreply.github.com>
2021-04-11 01:29:49 +0700

    create bottom navigation, divide app into fragments

--- app/src/main/java/com/example/musicplayerapp/MainActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.musicplayerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+//    private val bottomNavigation: BottomNavigationItemView = findViewById(R.id.bottomNavigation)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val mainFragment = MainFragment()
+        val profileFragment = ProfileFragment()
+        val favoriteListFragment = FavoriteListFragment()
+
+        loadFragment(mainFragment)
+
+        bottomNavigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.navigation_home -> {
+                    loadFragment(mainFragment)
+                    true
+                }
+                R.id.navigation_favoriteList -> {
+                    loadFragment(favoriteListFragment)
+                    true
+                }
+                R.id.navigation_profile -> {
+                    loadFragment(profileFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment, fragment)
+            .setCustomAnimations(R.anim.design_bottom_sheet_slide_in, R.anim.design_bottom_sheet_slide_out)
+            .addToBackStack(null)
+            .commit()
     }
 }
