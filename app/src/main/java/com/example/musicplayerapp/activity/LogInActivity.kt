68be5a52f7eac4a3d932b5a10d2ed73fe2b13760
missaut1309 <57839645+missaut1309@users.noreply.github.com>
2021-04-19 23:14:06 +0700

    registration and profile screen

---
+++ app/src/main/java/com/example/musicplayerapp/activity/LogInActivity.kt
@@ -0,0 +1,16 @@
+package com.example.musicplayerapp.activity
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicplayerapp.R
+import kotlinx.android.synthetic.main.activity_log_in.*
+
+class LogInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+
+
+    }
+}
--- app/src/main/java/com/example/musicplayerapp/activity/WelcomeActivity.kt
+++ app/src/main/java/com/example/musicplayerapp/activity/WelcomeActivity.kt
@@ -1,12 +1,24 @@
 package com.example.musicplayerapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.musicplayerapp.R
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        btn_login.setOnClickListener {
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
+
+        btn_sign_up.setOnClickListener {
+            val intent = Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
